77f34f7889d0b46d46c83c36630d5d088a74cbc0
xquery <best.highloyal@gmail.com>
2023-03-25 21:29:33 +0530

    added set_password in user serializer

--- iam/users/serializers.py
+++ iam/users/serializers.py
@@ -6,6 +6,17 @@ class UserSerializer(serializers.BaseUserSerializer):
 
     class Meta(serializers.BaseUserSerializer.Meta):
         model = models.User
-        exclude = serializers.BaseUserSerializer.Meta.exclude + (
-            'password',
-        )
+        exclude = serializers.BaseUserSerializer.Meta.exclude
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        instance = self.Meta.model(**validated_data)
+        assert password is not None
+        instance.set_password(password)
+        instance.save()
+        return instance
+
+
