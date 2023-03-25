40d66a0deb55a70841cfab5750a59347e47b313c
xquery <best.highloyal@gmail.com>
2023-03-25 13:30:25 +0530

    added tools/run_dev script and cache objects

---
+++ tools/run_dev.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+IAM_PORT=8000
+SOUTH_GERMAN_BANK_PORT=8001
+NOTIFICATIONS_PORT=8002
+MESSAGES_PORT=8003
+TRANSACTIONS_PORT=8004
+
+IAM_SERVICE="IAM"
+SOUTH_GERMAN_BANK_SERVICE="SOUTH_GERMAN_BANK"
+NOTIFICATIONS_SERVICE="NOTIFICATIONS"
+MESSAGES_SERVICE="MESSAGES"
+TRANSACTIONS_SERVICE="TRANSACTIONS"
+
+#../iam/manage.py runserver ${IAM_PORT}
+
+gnome-terminal --tab --title=${IAM_SERVICE} -e "bash -c '../iam/manage.py runserver ${IAM_PORT}'"
+gnome-terminal --tab --title=${SOUTH_GERMAN_BANK_SERVICE} -e "bash -c '../iam/manage.py runserver ${SOUTH_GERMAN_BANK_PORT}'"
