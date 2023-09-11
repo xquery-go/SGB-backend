db239e3b508afdf63ccdc8dec5c8931ddef37f35
xquery <best.highloyal@gmail.com>
2023-09-11 18:16:21 +0530

    unregistered Auth user and groups from IAM backends

---
+++ core/core/registry/reflection_services.py
@@ -0,0 +1,18 @@
+# from concurrent import futures
+# import grpc
+#
+# from grpc_reflection.v1alpha import reflection
+# from project_a import your_proto_file_a_pb2_grpc
+#
+# server = grpc.server(futures.ThreadPoolExecutor())
+#
+# your_proto_file_a_pb2_grpc.add_YourServiceServicer_to_server(
+#     YourServiceImplementationA(), server
+# )
+#
+# SERVICE_NAMES = (
+#     your_proto_file_a_pb2.DESCRIPTOR.services_by_name['YourService'].full_name,
+# )
+# reflection.enable_server_reflection(SERVICE_NAMES, server)
+# server.add_insecure_port('[::]:50051')  # You can specify the desired port
+# server.start()
--- iam/iam/backends.py
+++ iam/iam/backends.py
@@ -1,22 +1,26 @@
 from django.contrib.auth.backends import BaseBackend
 from rest_framework.exceptions import ValidationError as DRFValidationError
+from django.contrib.auth.models import User, Group
 
-from users.models import User
+from users.models import User as iam_user
+from django.contrib import admin
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 
 class CustomAuthBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
+            user = iam_user.objects.get(username=username)
+        except iam_user.DoesNotExist:
             return None
 
         if user.check_password(password):
-
             return user
 
     def get_user(self, user_id):
         try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
+            return iam_user.objects.get(pk=user_id)
+        except iam_user.DoesNotExist:
             return None
